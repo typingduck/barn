6d802b08be15e93ca140944c520a96eaf2052d4a
Omid Aladini <omidaladini@gmail.com>
2012-07-27 16:36:51 +0200

    Sync one file a time to preserve order of rotation

--- agent/sync.sh
+++ agent/sync.sh
@@ -2,8 +2,19 @@
 
 BARN_CONF="barn.conf"
 
-while inotifywait --exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~' --exclude '\.(swx|swp)' -q -e close_write logs; do
-  rsync --size-only --verbose --exclude '*u' --exclude 'config' --exclude 'current' --exclude 'lock' --exclude '*~' --exclude '\.(swx|swp)' logs/* rsync://`cat $BARN_CONF`/barn_logs | tee rsync.log
+INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~' --exclude '\.(swx|swp)'"
+
+RSYNC_EXCLUSIONS="--exclude '*u' --exclude 'config' --exclude 'current' --exclude 'lock' --exclude '*~' --exclude '\.(swx|swp)'"
+RSYNC_FLAGS="--size-only --verbose"
+RSYNC_SOURCE="logs/*"
+
+while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
+  CANDIDATES=$(rsync --list-only $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE $RSYNC_DEST | grep "@" | awk '{print $5}' | sort)
+  for c in $CANDIDATES; do
+    echo "Candidate is " + $c
+    rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS logs/$c rsync://`cat $BARN_CONF`/barn_logs | tee rsync.log
+  done
+  #rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE rsync://`cat $BARN_CONF`/barn_logs | tee rsync.log
   for i in $(cat rsync.log | grep @); do rm logs/$i; done
 done
 
