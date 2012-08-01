2cc481efe7ca4a5fa51fa59b06651f0b60aad385
Omid Aladini <omidaladini@gmail.com>
2012-08-01 17:16:45 +0200

    Fix rsync exclusion logic. Remove inotify because of OSX. Sync monotonically

--- agent/sync.sh
+++ agent/sync.sh
@@ -4,18 +4,25 @@ BARN_CONF="barn.conf"
 
 INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~' --exclude '\.(swx|swp)'"
 
-RSYNC_EXCLUSIONS="--exclude '*u' --exclude 'config' --exclude 'current' --exclude 'lock' --exclude '*~' --exclude '\.(swx|swp)'"
-RSYNC_FLAGS="--size-only --verbose"
+RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=lock --exclude=*~ --exclude=\.(swx|swp)"
+RSYNC_FLAGS="--verbose"
 RSYNC_SOURCE="logs/*"
 
-while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
+#while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
+
+# No inotify on OSX :(
+while true; do
+  sleep 1
+
+  echo "START OF RSYNC SESSION"
+
   CANDIDATES=$(rsync --list-only $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE $RSYNC_DEST | grep "@" | awk '{print $5}' | sort)
+
   for c in $CANDIDATES; do
-    echo "Candidate is " + $c
-    rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS logs/$c rsync://`cat $BARN_CONF`/barn_logs | tee rsync.log
+    echo "Candidate is " $c " rotated on  " $(echo $c | tai64nlocal)
+    rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS logs/$c rsync://`cat $BARN_CONF`/barn_logs
   done
-  #rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE rsync://`cat $BARN_CONF`/barn_logs | tee rsync.log
-  for i in $(cat rsync.log | grep @); do rm logs/$i; done
+
 done
 
 
