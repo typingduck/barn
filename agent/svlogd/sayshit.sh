388b2256b2702ccdb7cc5687467c52327bc17937
Omid Aladini <omidaladini@gmail.com>
2012-08-01 17:12:13 +0200

    Make sayshit to roughly spit controlled random logs

--- agent/svlogd/sayshit.sh
+++ agent/svlogd/sayshit.sh
@@ -1,2 +1,8 @@
-#cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 500
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 500
+#cat /dev/urandom | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 500
+size_in_mb=5
+sizein_bytes=$(($size_in_mb * 1024 * 1024))
+
+while true; do
+  dd if=/dev/urandom bs=$sizein_bytes count=1 | LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 500
+  sleep 1
+done
