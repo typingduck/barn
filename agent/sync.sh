cbce641a3c76d3f88f85ca96e7f8742ba753d1b8
Omid Aladini <omidaladini@gmail.com>
2012-08-14 18:22:58 +0200

    Support per-service sync-ing

--- agent/sync.sh
+++ agent/sync.sh
@@ -2,27 +2,25 @@
 
 BARN_CONF="barn.conf"
 
-INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~' --exclude '\.(swx|swp)'"
+INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~'"
 
-RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=lock --exclude=*~ --exclude=\.(swx|swp)"
-RSYNC_FLAGS="--verbose"
-RSYNC_SOURCE="logs/*"
+RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=lock --exclude=*~"
+RSYNC_FLAGS="-c --verbose"
+RSYNC_SOURCE_TREE="logs/*"
 
 #while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
 
 # No inotify on OSX :(
 while true; do
-  sleep 1
-
-  echo "START OF RSYNC SESSION"
-
-  CANDIDATES=$(rsync --list-only $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE $RSYNC_DEST | grep "@" | awk '{print $5}' | sort)
-
-  for c in $CANDIDATES; do
-    echo "Candidate is " $c " rotated on  " $(echo $c | tai64nlocal)
-    rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS logs/$c rsync://`cat $BARN_CONF`/barn_logs
+  for RSYNC_SOURCE in $RSYNC_SOURCE_TREE; do
+    BASE_NAME=$(basename $RSYNC_SOURCE)
+    CANDIDATES=$(rsync --dry-run $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE/* rsync://`cat $BARN_CONF`/barn_logs/$BASE_NAME/ | grep "@" | awk '{print $1}' | sort)
+    echo "CANDIDATES for $RSYNC_SOURCE: $CANDIDATES"
+    for c in $CANDIDATES; do
+      echo "Candidate is $c"
+      rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE/$c rsync://`cat $BARN_CONF`/barn_logs/$BASE_NAME/
+    done
   done
-
 done
 
 
