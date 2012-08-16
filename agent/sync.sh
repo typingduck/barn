c7bdf298c3b8e295c9c56a2d87f7ce7f2d2a985d
Omid Aladini <omidaladini@gmail.com>
2012-08-16 18:08:23 +0200

    Remove barn.conf and accept as param. Make bash script more robust

--- agent/sync.sh
+++ agent/sync.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-BARN_CONF="barn.conf"
+if [[ "$1" == "" || "$2" == "" ]]; then
+  echo "Usage: sync.sh RSYNC_HOST:RSYNC_PORT RSYNC_LOG_TREE";
+  exit 0;
+fi
+
+BARN_RSYNC_ADDR=$1
 
 INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclude '*~'"
 
 RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=lock --exclude=*~"
-RSYNC_FLAGS="-c --verbose"
-RSYNC_SOURCE_TREE="logs/*"
+RSYNC_FLAGS="-c --verbose"  # --verbose is important since we use it to issue rsync incrementally
+RSYNC_LOG_TREE="$2"
 
 #while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
 
 # No inotify on OSX :(
 while true; do
-  for RSYNC_SOURCE in $RSYNC_SOURCE_TREE; do
+  for RSYNC_SOURCE in `ls "$RSYNC_LOG_TREE"`; do
     BASE_NAME=$(basename $RSYNC_SOURCE)
-    CANDIDATES=$(rsync --dry-run $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE/* rsync://`cat $BARN_CONF`/barn_logs/$BASE_NAME/ | grep "@" | awk '{print $1}' | sort)
-    echo "CANDIDATES for $RSYNC_SOURCE: $CANDIDATES"
+
+    echo "Checking for $RSYNC_LOG_TREE$RSYNC_SOURCE.. "
+
+    RSYNC_COMMAND_LINE="$RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_LOG_TREE$RSYNC_SOURCE/* rsync://$BARN_RSYNC_ADDR/barn_logs/$BASE_NAME/"
+    CANDIDATES=$(eval "rsync --dry-run $RSYNC_COMMAND_LINE" | grep "@" | awk '{print $1}' | sort)
     for c in $CANDIDATES; do
       echo "Candidate is $c"
-      rsync $RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE/$c rsync://`cat $BARN_CONF`/barn_logs/$BASE_NAME/
+      rsync $RSYNC_COMMAND_LINE
     done
   done
 done
