e0d48b0a840faee9f81d1e46c1d4c3dde3f0055a
Omid Aladini <omidaladini@gmail.com>
2012-08-20 18:07:42 +0200

    Flush not more than every minute [test]

--- server/hdfs-writer/src/main/scala/test.scala
+++ server/hdfs-writer/src/main/scala/test.scala
@@ -14,6 +14,7 @@ import org.apache.hadoop.io.{BytesWritable => BW, LongWritable => LW, SequenceFi
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{Path, LocalFileSystem, RawLocalFileSystem}
 import org.apache.hadoop.io.SequenceFile
+import org.joda.time._
 
 object BarnHdfsWriter extends App {
 
@@ -25,6 +26,15 @@ object BarnHdfsWriter extends App {
   val rootLogPath = remainingArgs(0)
   val exclude = List(".gitignore", "target", "lock", "current")  //Dev, remove me
   val baseHdfsPath = "/user/omid/barn-test/"
+
+  def timeToFlush(lastWrittenDate: DateTime) : Boolean = {
+    System.out.println("lastWrittenDate: " + lastWrittenDate)
+    System.out.println("            now: " + DateTime.now)
+    !new Interval(lastWrittenDate, DateTime.now(DateTimeZone.UTC)).toDuration
+                                              .toStandardSeconds
+                                              .isLessThan(Seconds.seconds(60))
+  }
+
 
   while(true) {
 
@@ -40,23 +50,37 @@ object BarnHdfsWriter extends App {
     servicesDirs.foreach { service : File =>
       System.out.println("service : " + service)
 
-      service.listFiles.toList.filterNot(x => exclude.contains(x.getName)).sorted match {
+      service.listFiles
+             .toList
+             .filterNot(x => exclude.contains(x.getName))
+             .sorted match {
+
         case null =>
           System.out.println("ERROR: For some reason I can't list files in " + service)
+
         case localFiles =>
 
+          //Service log path on HDFS
           val remotePath = new Path(baseHdfsPath + service.getName)
 
           //Make sure the path is there
           Hdfsfsfsfs.createPath(fs, remotePath)
+
+          //Grab list of files on HDFS
           val remoteFiles = fs.listStatus(remotePath).toList.map(_.getPath.getName).sorted
 
           (remoteFiles.lastOption match {
-            case Some(last) => localFiles.dropWhile(_.getName <= last) match {
-              case Nil => None
-              case x => Some(x)
-            }
-            case _ => localFiles.lastOption.map(_ => localFiles)
+            case Some(last) =>
+              val lastTimestamp = Tai64.convertTai64ToTime(last.drop(1))
+              System.out.println("Last written date: " + last)
+              localFiles.dropWhile(_.getName <= last) match {
+                case Nil => None
+                case x if timeToFlush(lastTimestamp) => Some(x)
+                case _ =>
+                  System.out.println("Nope, it's a bit of time ago I flushed. Next time!")
+                  None
+              }
+            case _ => Some(localFiles)
           }).map { candidates =>
               val combinedName = "/tmp/" + candidates.last.getName
 
