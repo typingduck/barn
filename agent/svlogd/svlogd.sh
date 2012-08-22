beb87c3f1a456a101fa70effea7045cb1000142a
Omid Aladini <omidaladini@gmail.com>
2012-08-22 19:34:51 +0200

    Dynamically generate config file. Will ask for params to customize it later

--- agent/svlogd/svlogd.sh
+++ agent/svlogd/svlogd.sh
@@ -7,5 +7,12 @@ fi
 
 LOG_DIRECTORY=$1$2
 
+cat >$LOG_DIRECTORY/config <<EOL
+#Dynamically generated, your changes will be overwritten and ignored
+s1000000
+n100
+t3600
+EOL
+
 mkdir -p $LOG_DIRECTORY
 svlogd -v -tt $LOG_DIRECTORY
