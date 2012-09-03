935a5bb1e85ae9bac73d6a24bffe6ceb31ad7c14
Omid Aladini omid@soundcloud.com <omid@ams-db025.int.s-cloud.net>
2012-09-03 16:43:18 +0000

    Adds inotify for Linux machines. Fixes globbing.

--- agent/sync.sh
+++ agent/sync.sh
@@ -14,23 +14,34 @@ RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=loc
 RSYNC_FLAGS="-c --verbose"  # --verbose is important since we use it to issue rsync incrementally
 RSYNC_LOG_TREE="$2"
 
-#while inotifywait $INOTIFY_EXCLUSIONS -q -e close_write logs; do
+uname=`uname`                                                                   
+if [[ "$uname" == 'Linux' ]]; then                                              
+   SLEEP_COMMAND="inotifywait $INOTIFY_EXCLUSIONS --timeout 10 -q -e close_write $RSYNC_LOG_TREE/"
+elif [[ "$uname" == 'Darwin' ]]; then                                           
+   echo "I'm on OSX so I'm going to loop like crazy. Use Linux for inotify."       
+   SLEEP_COMMAND="sleep 10"                                                        
+fi
 
-# No inotify on OSX :(
-while true; do
-  for RSYNC_SOURCE in `ls "$RSYNC_LOG_TREE"`; do
-    BASE_NAME=$(basename $RSYNC_SOURCE)
+while true; do  # will sleep inside the loop
+  for RSYNC_SOURCE in `find $RSYNC_LOG_TREE -type d`; do
+    BASE_NAME=$(echo $RSYNC_SOURCE | sed 's/\//_/g')
     HOST_NAME=$(hostname -f)
 
-    echo "Checking for $RSYNC_LOG_TREE$RSYNC_SOURCE.. "
+    echo "Checking for $RSYNC_SOURCE"
 
-    RSYNC_COMMAND_LINE="$RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_LOG_TREE$RSYNC_SOURCE/* rsync://$BARN_RSYNC_ADDR/barn_logs/$BASE_NAME@$HOST_NAME/"
-    CANDIDATES=$(eval "rsync --dry-run $RSYNC_COMMAND_LINE" | grep "@" | awk '{print $1}' | sort)
+    RSYNC_COMMAND_LINE="$RSYNC_FLAGS $RSYNC_EXCLUSIONS $RSYNC_SOURCE/* rsync://$BARN_RSYNC_ADDR/barn_logs/$BASE_NAME@$HOST_NAME/"
+    CANDIDATES=$(eval "rsync --dry-run $RSYNC_COMMAND_LINE" | grep -v "created directory" | grep "@" | awk '{print $1}' | sort)
     for c in $CANDIDATES; do
       echo "Candidate is $c"
       rsync $RSYNC_COMMAND_LINE
     done
   done
+
+  if [[ $CANDIDATES == "" ]]; then
+    echo "Nothing more to sync. Hibernating till a log file is rotated."
+    $SLEEP_COMMAND
+  fi
+
 done
 
 
