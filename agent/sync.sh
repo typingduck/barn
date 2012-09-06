bdb93ed3d2615f3ff85860e0c0065bfa23952354
Omid Aladini <omidaladini@gmail.com>
2012-09-06 18:54:03 +0200

    Support sync-ing multi category logs

--- agent/sync.sh
+++ agent/sync.sh
@@ -11,8 +11,10 @@ INOTIFY_EXCLUSIONS="--exclude '\.u' --exclude 'lock' --exclude 'current' --exclu
 RSYNC_EXCLUSIONS="--exclude=*.u --exclude=config --exclude=current --exclude=lock --exclude=*~"
 RSYNC_FLAGS="-c --verbose"  # --verbose is important since we use it to issue rsync incrementally
 RSYNC_LOG_TREE=$2
+RSYNC_LOG_TREE_EXCLUDE="supervise" # Runit specific # FIXME
 
-# For exmample for sv: 's/\/etc\/service\/\(.*\)\/log\/main/\1/'
+# For exmample for sv: 's/\/etc\/sv\/\(.*\)\/log\/\(.*\)/\1-\2/'
+# Extracts a name from paths like /etc/sv/*/log/*
 # Or for any basename match: 's#.*/##'
 SERVICE_NAME_SED_EXPRESSION=$3
 
@@ -68,7 +70,7 @@ function sync {
 }
 
 # Main program is here
-for RSYNC_SOURCE in `find $RSYNC_LOG_TREE -type d`; do
+for RSYNC_SOURCE in `find $RSYNC_LOG_TREE -type d -and -not -name $RSYNC_LOG_TREE_EXCLUDE`; do
   (
     while true; do
       sync $RSYNC_SOURCE || sleepit $RSYNC_SOURCE
