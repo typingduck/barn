f4d9fb267c252c4b88004ce3dd8311a017426dcd
Omid Aladini <omidaladini@gmail.com>
2013-06-25 16:03:53 +0200

    Handle a case that empty list was passed to the combiner

--- barn-hdfs/src/main/scala/barn/main.scala
+++ barn-hdfs/src/main/scala/barn/main.scala
@@ -135,9 +135,13 @@ object BarnHdfsWriter
             case x => x success
           }
       case None =>
-	localFiles.dropWhile(f =>    //TODO Deduplicate me with the case above
-	  Tai64.convertTai64ToTime(
-	    svlogdFileNameToTaiString(f getName)).isBefore(maxLookBackTime)) success
+        localFiles.dropWhile(f =>    //TODO Deduplicate me with the case above
+          Tai64.convertTai64ToTime(
+            svlogdFileNameToTaiString(f getName))
+              .isBefore(maxLookBackTime)) match {
+                case Nil => NothingToSync("No local files left to sync.") fail
+                case x => x success
+               }
     }
   }
 }
